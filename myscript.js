e58c74cbf28cf63b29ae8241b7920e41e50c4658
Rashid Rayhan <58268912+RashidRayhan@users.noreply.github.com>
2026-02-05 19:16:05 +0600

    Javascript Syntax

--- myscript.js
+++ myscript.js
@@ -1,3 +1,5 @@
 function myFunction(){
-    document.getElementById(newdemo).innerHTML ="Change Text"
+    document.getElementById("newdemo").innerHTML ="Change Text"
 }
+
+    document.getElementById("number").innerHTML = 105.5;
